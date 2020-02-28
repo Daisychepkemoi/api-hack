07741e4c29e7ffdb09921a634c886b593d416366
Daisy Chepkemoi <dchepkemoi408@gmail.com>
2020-02-28 07:04:26 +0300

    added authentication and created some api endpoints

--- app/Http/Controllers/OrderDetailController.php
+++ app/Http/Controllers/OrderDetailController.php
@@ -7,79 +7,127 @@ use Illuminate\Http\Request;
 
 class OrderDetailController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        // $user = auth()->user();
+
+        $Questions = Questions::with(['Answers', 'Comments'])->get();
+
+        return $this->sendResponse($Questions->toArray(), 'Questions retrieved successfully.');
+    }
+    public function createQuestion(Request $request){
+     $input = $request->all();
+        $validator = Validator::make($input, [
+            'body' => 'required',
+            // 'users_id' => 'required'
+        ]);
+        if ($validator->fails())
+          {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        $question = new Questions;
+        $question->users_id = auth()->user()->id;
+        $question->body = $request->body;
+        $question->save();
+
+        return response()->json(["message" => "question record created"], 200);
+    }
+   
+   public function updateQuestion(Request $request,$id) {
+           
+
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'body' => 'required'
+        ]);
+         if ($validator->fails())
+          {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        $userid = Questions::where('id',$id)->first();
+        // dd(Questions::where('id',$id)->first()->users_id);
+         if (Questions::where('id', $id)->exists())
+            {
+                if(auth()->user()->id == $userid->users_id)
+                {
+                $Question = Questions::where('id', $id)->find($id);
+                $Question->body = $input['body'];
+                $Question->save();
+                return response()->json([$Question, "message" => "Question updated successfully"], 200); 
+                 }
+        
+            else{
+                 return response()->json(["message" => " Unauthorized "], 401);
+                }
+        }
+          else{
+            return response()->json(["message" => "Question not found"], 404);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Order_Detail  $order_Detail
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Order_Detail $order_Detail)
+    public function show($id)
     {
-        //
+        $Question = Questions::find($id);
+
+
+        if (is_null($Question)) {
+            return response()->json(["message" => "Question not found"], 404);
+        }
+         $questionone = Questions::where('id',$id)->with(['Answers', 'Comments'])->get();
+
+        return $this->sendResponse($questionone->toArray(), 'Questions retrieved successfully.');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Order_Detail  $order_Detail
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order_Detail $order_Detail)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Order_Detail  $order_Detail
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Order_Detail $order_Detail)
-    {
-        //
-    }
+ 
+
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Order_Detail  $order_Detail
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Order_Detail $order_Detail)
+    public function destroy($id)
     {
-        //
+        $user=auth()->user();
+        $question = Questions::find($id);
+
+        if (is_null($question)) {
+            return $this->sendError('Question not found.');
+        }
+        else{
+
+             if(auth()->user()->id == Questions::where('id',$id)->first()->users_id)
+                {
+                    $Comments =  Comments::where('questions_id', $id)->delete();
+                    $Answers =  Answers::where('questions_id', $id)->delete();
+                    $Question = Questions::where('id', $id)->delete();
+                    return $this->sendResponse('', 'Questions deleted successfully.');
+     
+                }
+            else
+            {
+                return response()->json(["message" => "Unauthorized"], 401);
+
+            }
+        }
+       
+        //*check on it later*
+
     }
 }
